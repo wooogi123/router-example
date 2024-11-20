d69ef87b6d67d1c7534c1081b14138c922cf5a67
Akalee <wooogi123@gmail.com>
2024-11-21 01:22:30 +0900

    feat: define history interface

--- packages/history/src/index.ts
+++ packages/history/src/index.ts
@@ -1,1 +1,26 @@
+export type Action = 'push' | 'pop' | 'replace';
 
+export type Location = string;
+
+export type To = string;
+
+export interface ChangeEvent {
+  action: Action;
+  location: Location;
+  delta: number;
+}
+
+export type Listener = (event: ChangeEvent) => void;
+
+export interface History {
+  get stack(): Location[];
+  get location(): Location;
+
+  push(to: To): void;
+  pop(to: To): void;
+  replace(to: To): void;
+
+  go(delta: number): void;
+
+  addListener(listener: Listener): () => void;
+}
